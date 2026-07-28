46501593c32f29aed7836f619a839c789b19efd1
Divyanshu <srivastavadivyanshu889@gmail.com>
2026-07-28 13:44:09 +0530

    Remove embedding_dim hardcore

--- mini_gpt/positional_encoding.py
+++ mini_gpt/positional_encoding.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 import math
 class PositionalEncoding(nn.Module):
-    def __init__(self,embedding_dim,max_length=5000):
+    def __init__(self,embedding_dim=256,max_length=5000):
         super().__init__()
         pe = torch.zeros(max_length,embedding_dim)
         position = torch.arange(max_length).unsqueeze(1)
@@ -12,7 +12,7 @@ class PositionalEncoding(nn.Module):
             2,
             dtype=torch.float
         )*(-math.log(10000)/embedding_dim))
-        pe[:, 0::2] = torch.sin(position*div_term)
+        pe[:, 0::2] = torch. sin(position*div_term)
         pe[:, 1::2] = torch.cos(position*div_term)
         pe.unsqueeze(0)
         self.register_buffer("pe",pe)

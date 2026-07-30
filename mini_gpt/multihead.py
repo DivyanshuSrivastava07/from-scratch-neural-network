52c6ffe946161c6259f62edf24e4f474c027ace7
Divyanshu <srivastavadivyanshu889@gmail.com>
2026-07-30 08:13:37 +0530

    Masking added

--- mini_gpt/multihead.py
+++ mini_gpt/multihead.py
@@ -1,7 +1,7 @@
 import torch.nn as nn,torch
 import math
 class Multihead(nn.Module):
-    def __init__(self,embedding_dim,heads):
+    def __init__(self,embedding_dim,heads,max_seq_len):
         super().__init__()
         self.heads = heads
         self.embedding_dim = embedding_dim
@@ -12,9 +12,18 @@ class Multihead(nn.Module):
             embedding_dim,
             embedding_dim
         )
+        self.mask = self.register_buffer(
+            "mask",
+            torch.tril(
+                torch.ones_like(
+                    max_seq_len,max_seq_len
+                )
+            )
+        )
     def forward(self,x : torch.Tensor) -> torch.Tensor:
         qkv = self.qkv(x)
         B, S, E = x.shape
+        mask = self.mask[:S, :S]
         Q, K, V = qkv.chunk(3,dim=-1)
 
         Q = Q.view(B,S,self.heads,self.head_dim).transpose(2,1)
@@ -22,8 +31,19 @@ class Multihead(nn.Module):
         V = V.view(B,S,self.heads,self.head_dim).transpose(2,1)
 
         scores = (Q @ K.transpose(-2,-1))/math.sqrt(self.head_dim)
+        scores = scores.masked_fill(
+            mask==0,
+            float("-inf")
+        )
         weights = torch.softmax(scores,dim=-1)
-        self.attention = weights
+
+        # if(self.training):
+        #     self.attention = weights        #### Usually we only store attention weights when debugging or visualizing.
+        weights = self.attn_dropout(weights)
+
+        """During training, some attention probabilities are randomly zeroed out.
+        This prevents the model from becoming overly dependent on a few specific attention paths,
+        improving generalization."""
         out = weights @ V
         out = out.transpose(2,1).contiguous().view(B,S,E)
         out = self.out(out)

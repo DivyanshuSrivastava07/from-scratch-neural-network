bd2f7d923942a2255cca82c9a5fb63d45e214a8f
Divyanshu <srivastavadivyanshu889@gmail.com>
2026-07-31 04:35:38 +0530

    Removed Hardcoded hidden_dim

--- mini_gpt/feedforward.py
+++ mini_gpt/feedforward.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 class FeedForward(nn.Module):
-    def __init__(self,embedding_dim=356):
+    def __init__(self,embedding_dim=256,hidden_dim=4*256):
         super().__init__()
         self.ffn = nn.Sequential(
-            nn.Linear(embedding_dim,4*embedding_dim),
+            nn.Linear(embedding_dim,hidden_dim),
             nn.GELU(),
-            nn.Linear(4*embedding_dim,embedding_dim)
+            nn.Linear(4*embedding_dim,hidden_dim)
         )
     def forward(self,x):
         return self.ffn(x)
